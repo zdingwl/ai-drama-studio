b8232e40acd0d64bfd3e109db1e3bf581049fe59
zdingwl <139205911+zdingwl@users.noreply.github.com>
2026-08-24 15:38:55 +0800

    feat: add F05 workbench route

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -4,26 +4,16 @@ import ProjectWorkspace from '../views/ProjectWorkspace.vue'
 import SourceVideoImport from '../views/SourceVideoImport.vue'
 import VideoPreprocess from '../views/VideoPreprocess.vue'
 import ShotDetection from '../views/ShotDetection.vue'
+import ShotWorkbench from '../views/ShotWorkbench.vue'
 
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', name: 'projects', component: ProjectHome },
     { path: '/projects/:projectId', name: 'workspace', component: ProjectWorkspace },
-    {
-      path: '/projects/:projectId/source-video',
-      name: 'source-video',
-      component: SourceVideoImport,
-    },
-    {
-      path: '/projects/:projectId/preprocess',
-      name: 'preprocess',
-      component: VideoPreprocess,
-    },
-    {
-      path: '/projects/:projectId/shot-detection',
-      name: 'shot-detection',
-      component: ShotDetection,
-    },
+    { path: '/projects/:projectId/source-video', name: 'source-video', component: SourceVideoImport },
+    { path: '/projects/:projectId/preprocess', name: 'preprocess', component: VideoPreprocess },
+    { path: '/projects/:projectId/shot-detection', name: 'shot-detection', component: ShotDetection },
+    { path: '/projects/:projectId/shot-workbench', name: 'shot-workbench', component: ShotWorkbench },
   ],
 })
