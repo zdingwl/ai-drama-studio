c78974b547e561a7455363b9f9b15d5723b76e65
zdingwl <139205911+zdingwl@users.noreply.github.com>
2026-08-24 10:04:48 +0800

    feat(F02 UI): register source video import route

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ProjectHome from '../views/ProjectHome.vue'
 import ProjectWorkspace from '../views/ProjectWorkspace.vue'
+import SourceVideoImport from '../views/SourceVideoImport.vue'
 
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', name: 'projects', component: ProjectHome },
     { path: '/projects/:projectId', name: 'workspace', component: ProjectWorkspace },
+    {
+      path: '/projects/:projectId/source-video',
+      name: 'source-video',
+      component: SourceVideoImport,
+    },
   ],
 })
