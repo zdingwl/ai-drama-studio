e57afb35c9e5c05777962c553c3c4ecd0f028f65
zdingwl <139205911+zdingwl@users.noreply.github.com>
2026-09-03 10:38:42 +0800

    feat: route project list to rebuilt V5 page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import ProjectListV4 from '../views/ProjectListV4.vue'
+import ProjectListV5 from '../views/ProjectListV5.vue'
 import ProjectStudioV4 from '../views/ProjectStudioV4.vue'
 
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', name: 'projects', component: ProjectListV4 },
+    { path: '/', name: 'projects', component: ProjectListV5 },
     { path: '/projects/:projectId', name: 'studio', component: ProjectStudioV4 },
     { path: '/:pathMatch(.*)*', redirect: '/' },
   ],
