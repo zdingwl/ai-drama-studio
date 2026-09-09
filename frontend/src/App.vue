52d78d4754d2aa9fd2e0930c990ca6a7575a5cf1
zdingwl <139205911+zdingwl@users.noreply.github.com>
2026-09-09 09:31:26 +0800

    P7: surface SOURCE_BIBLE acceptance in workspace

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,6 +3,7 @@ import { useRoute } from 'vue-router'
 
 import AppShell from '@/components/AppShell.vue'
 import P6AcceptancePanel from '@/components/P6AcceptancePanel.vue'
+import P7SourceBiblePanel from '@/components/P7SourceBiblePanel.vue'
 
 const route = useRoute()
 </script>
@@ -11,5 +12,6 @@ const route = useRoute()
   <AppShell>
     <RouterView />
     <P6AcceptancePanel v-if="route.name === 'project-workspace'" />
+    <P7SourceBiblePanel v-if="route.name === 'project-workspace'" />
   </AppShell>
 </template>
