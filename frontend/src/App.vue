3796b1f3b34c633e099432928f39d9a73896cf91
zdingwl <139205911+zdingwl@users.noreply.github.com>
2026-09-10 14:22:06 +0800

    feat: mount P9 source resolution workspace

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,6 +5,7 @@ import AppShell from '@/components/AppShell.vue'
 import P6AcceptancePanel from '@/components/P6AcceptancePanel.vue'
 import P7SourceUnderstandingWorkspace from '@/components/P7SourceUnderstandingWorkspace.vue'
 import P8ShotBreakdownPanel from '@/components/P8ShotBreakdownPanel.vue'
+import P9SourceResolutionPanel from '@/components/P9SourceResolutionPanel.vue'
 
 const route = useRoute()
 </script>
@@ -15,5 +16,6 @@ const route = useRoute()
     <P6AcceptancePanel v-if="route.name === 'project-workspace'" />
     <P7SourceUnderstandingWorkspace v-if="route.name === 'project-workspace'" />
     <P8ShotBreakdownPanel v-if="route.name === 'project-workspace'" />
+    <P9SourceResolutionPanel v-if="route.name === 'project-workspace'" />
   </AppShell>
 </template>
